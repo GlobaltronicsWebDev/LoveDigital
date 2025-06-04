5c068d2337b6859e1b054ce21b05b417f5a51a64
Web Developer / MIS <97711236+GlobaltronicsWebDev@users.noreply.github.com>
2025-06-04 10:28:56 +0800

    Update Temp1Reg.php

--- Temp1Reg.php
+++ Temp1Reg.php
@@ -24,31 +24,12 @@ if(isset($_POST['submit'])){
   	$insert_data = "INSERT INTO user (id,name,email,phone,address,image,Template,date) VALUES ('$id','$name','$email','$phone','$address','$image','Template_1',NOW())";
   	$run_data = mysqli_query($con,$insert_data);
 
-if($run_data){
-        // Send data to Google Sheets via Google Apps Script Web App URL
-        $googleSheetUrl = 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec'; // Replace with your Google Apps Script URL
+  	if($run_data){
+  		header('location:Thankyou.php'); //change the Location to Thankyou Create your own thankyou page
+  	}else{
+  		echo "Data not insert";
+  	}
 
-        $postData = array(
-            'id' => $id,
-            'name' => $name,
-            'email' => $email,
-            'phone' => $phone,
-            'address' => $address,
-            'image' => $image
-        );
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $googleSheetUrl);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
-        curl_close($ch);
-
-        header('location:Thankyou.php'); //change the Location to Thankyou Create your own thankyou page
-    }else{
-        echo "Data not insert";
-    }
 }
 
 ?>
